55d99584f6429c5c5306a4b52c69a63d3e62724e
Orandi Harris <idnarosirrah@gmail.com>
2018-04-01 22:16:09 -0500

    Added comments

--- application/controllers/Form_input.php
+++ application/controllers/Form_input.php
@@ -10,24 +10,26 @@
 	</div>
 	<p class="text-success display-2">Small Step Operational Semantics</p>
 	<?php
+	# Programmer:Orandi Harris
 	class Form_input extends CI_Controller{
 
 		
 		public function processData()
 		{
-			# code...
-			$this->load->helper('url');
-			$home = base_url();
-			$form_data = $this->input->post();
+			# Controller to handle form input
+			$this->load->helper('url'); //Needed to access file path of project
+			$home = base_url();//Returns the base url of this project(localhost/CIAPLBlog)
+			$form_data = $this->input->post(); //Get form content
 			echo "<p class='display-4'>$form_data[input1]</p><br/>";
-			$data = (explode(" ", $form_data["input1"]));
+			$data = (explode(" ", $form_data["input1"])); //The explode function returns an array
 
 			$index = 0;
 			$leftHandExp = "";
 			$tempResult = 0;
 			$result = 0;
+			//Parse through array for specific characters
 				echo "<p class='display-4'>< x,σ > -> ".$data[$index]." &nbsp;&nbsp;&nbsp;&nbsp; < y,σ > ->". $data[$index + 2]."</p><br/>";
-
+			//Look at left hand side of expression at the index of 1 for a specific sign
 					if($data[$index + 1] == '+'){
 						$tempResult = $data[$index] + $data[$index + 2];
 						$string1 = "x + y";
@@ -68,6 +70,7 @@
 							$leftHandExp = $string1;
 							
 						}
+					//Look at the right hand side of expression for a particular sign
 						if($data[$index + 3] == '-'){
 							echo "<p class='display-4'> < z,σ > -> ". $data[$index + 4]."</p>";
 							$string2 = "- z";
@@ -101,17 +104,16 @@
 							echo " <p class='display-4'> < ( ".$leftHandExp. ") ".$string2. " ,σ> -> ".$tempResult." / ".$data[$index + 4]."</p><br/>";
 							echo "<p class='display-4'> < (" .$leftHandSide. ") / ".$data[$index + 4]. " ,σ> -> ".$result."</p>";
 		
-
+					//if no third argument was given
 					}else if(empty($data[$index + 3])){
 						echo "No third argument given.<br/>";
 					}
+					//Display final result
 					else {
 						echo "<br/>";
 						echo " <p class = 'display-4'> < (".$string1.") ".$string2.",σ>"." ⇓ ".$result."</p>";
 					}
 
-					
-					
 			echo "<button><a href=$home>Return to home page</a></button>";
 		}
 
--- application/controllers/Form_reader.php
+++ application/controllers/Form_reader.php
@@ -11,20 +11,21 @@
 	<p class="text-success display-2">Big Step Operational Semantics</p>
 	<?php
 	class Form_reader extends CI_Controller{
-
+	# Programmer:Orandi Harris
 		
 		public function processInput()
 		{
-			# code...
-			$this->load->helper('url');
-			$home = base_url();
-			$form_data = $this->input->post();
+			# Controller to handle form input
+			$this->load->helper('url');//Needed to access file path of project
+			$home = base_url();//Returns the base url of this project(localhost/CIAPLBlog)
+			$form_data = $this->input->post();//Get form content
 			echo "<p class='display-4'>$form_data[input1]</p><br/>";
 			$data = (explode(" ", $form_data["input1"]));
 
 			$index = 0;
+			//Parse through array for specific characters
 				echo "<p class='display-4'>< x,σ > ⇓ ".$data[$index]." &nbsp;&nbsp;&nbsp;&nbsp; < y,σ > ⇓". $data[$index + 2]."</p><br/>";
-
+				//Look at left hand side of expression at the index of 1 for a specific sign
 					if($data[$index + 1] == '+'){
 						$tempResult = $data[$index] + $data[$index + 2];
 						$string1 = "x + y";
@@ -43,18 +44,19 @@
 						echo "<p class='display-4'> < x - y,σ > ⇓ " .$tempResult."</p>";
 						
 					}
-						if($data[$index + 1] == '/'){
+					if($data[$index + 1] == '/'){
 							$tempResult = $data[$index] / $data[$index + 2];
 							$string1 = "x / y";
 							echo "<p class='display-4'> < x / y,σ > ⇓ " .$tempResult."</p>";
 							
-						}
-						if($data[$index + 3] == '-'){
+					}
+					//Look at the right hand side of expression for a particular sign
+					if($data[$index + 3] == '-'){
 							echo "<p class='display-4'> < z,σ > ⇓ ". $data[$index + 4]."</p>";
 							$string2 = "- z";
 							$result = $tempResult - $data[$index + 4];
 							
-						}
+					}
 
 					if($data[$index + 3] == '+'){
 						$string2 = "+ z";
@@ -74,17 +76,15 @@
 						echo "<p class='display-4'> < z,σ > ⇓ ". $data[$index + 4]."</p>";
 						$result = $tempResult / $data[$index + 4];
 						$index = 5;
+					//if no third argument was given
 					}else if(empty($data[$index + 3])){
 						echo "No third argument given.<br/>";
 					}
 					else {
-						# code...
+					//Display final result
 						echo "<br/>";
 						echo " <p class = 'display-4'> < ".$string1." ".$string2.",σ>"." ⇓ ".$result."</p>";
 					}
-
-					
-					
 			echo "<button><a href=$home>Return to home page</a></button>";
 		}
 
